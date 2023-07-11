530a23503352acbb65bea46a16c50514685bab56
Kof'i Knowles <JemalKnowles1@gmail.com>
2023-07-11 12:37:44 -0500

    first commit

---
+++ albums_migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE albums (
+                        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        artist VARCHAR(255),
+                        name VARCHAR(255),
+                        release_date INT,
+                        sales FLOAT,
+                        genre VARCHAR(255)
+);
+
